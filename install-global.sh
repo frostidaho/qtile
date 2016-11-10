b0be7e10ea3c4dccd8cd90c6978bbe516c294464
Idaho Frost <frostidaho@gmail.com>
2016-11-09 21:33:20 -0500

    Add install-global.sh script

--- install-global.sh
+++ install-global.sh
@@ -2,5 +2,5 @@
 # Getting script directory: http://stackoverflow.com/a/246128
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-pip3 install --user --editable "$DIR"
+sudo pip3 install --no-deps "$DIR"
 sudo cp "$DIR/resources/qtile.desktop" /usr/share/xsessions/qtile.desktop
