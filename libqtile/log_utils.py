e9b8227d7df9d6e8003fbe48c3aaf7bf6d817c56
Idaho Frost <frostidaho@gmail.com>
2017-02-16 18:19:43 -0500

    Refactor log_utils.py

--- libqtile/log_utils.py
+++ libqtile/log_utils.py
@@ -36,54 +36,52 @@ LOG_DIR = os.path.expanduser(os.path.join(
 DEFAULT_LOG_PATH = os.path.join(LOG_DIR, 'qtile.log')
 TESTS_LOG_PATH = os.path.join(LOG_DIR, 'tests_qtile.log')
 
+def _mkdir_p(path):
+    """Create directory at path. Does not raise exception if it already exists.
+
+    Found at: http://stackoverflow.com/a/600612
+    """
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+def _init_log(log_level, *handlers):
+    "Set logger to log_level & add handlers"
+    if not logger.hasHandlers():
+        for handler in handlers:
+            logger.addHandler(handler)
+    logger.setLevel(log_level)
+    # Capture everything from the warnings module.
+    logging.captureWarnings(True)
+    warnings.simplefilter("always")
+    logger.warning('Starting logging for Qtile with {}'.format(logger.handlers))
+    return logger
 
 def init_log(log_level=logging.WARNING, path=DEFAULT_LOG_PATH,
-             stream_handler=False, *other_handlers):
+             stdout=False, *other_handlers):
     """Initialize & return Qtile's root logger
 
     If path is given log will be written to the file at path.
-    If stream_handler is True log will be written to stdout.
+    If stdout is True log will be written to stdout.
 
     other_handlers should behave like those found in logging.handlers
     """
-    def mkdir_p(path):
-        """Create directory at path. Does not raise exception if it already exists.
-
-        Found at: http://stackoverflow.com/a/600612
-        """
-        try:
-            os.makedirs(path)
-        except OSError as exc:  # Python >2.5
-            if exc.errno == errno.EEXIST and os.path.isdir(path):
-                pass
-            else:
-                raise
-
-    def _init_log(log_level, *handlers):
-        "Set logger to log_level & add handlers"
-        if not logger.hasHandlers():
-            for handler in handlers:
-                logger.addHandler(handler)
-        logger.setLevel(log_level)
-        # Capture everything from the warnings module.
-        logging.captureWarnings(True)
-        warnings.simplefilter("always")
-        # msg = 'Starting logging for Qtile with {} handlers'
-        # logger.warning(msg.format(len(logger.handlers)))
-        logger.warning('Starting logging for Qtile with {}'.format(logger.handlers))
-        return logger
 
     all_handlers = []
-    if stream_handler or path:
+    if stdout or path:
         default_formatter = logging.Formatter(
             "%(asctime)s %(levelname)s %(name)s %(filename)s:%(funcName)s():L%(lineno)d %(message)s"
         )
-    if stream_handler:
-        shandler = logging.StreamHandler(stream=sys.stdout)
-        shandler.setFormatter(default_formatter)
-        all_handlers.append(shandler)
+    if stdout:
+        stream_handler = logging.StreamHandler(stream=sys.stdout)
+        stream_handler.setFormatter(default_formatter)
+        all_handlers.append(stream_handler)
     if path:
-        mkdir_p(os.path.dirname(path))
+        _mkdir_p(os.path.dirname(path))
         file_handler = RotatingFileHandler(
             path,
             maxBytes=int(2E6),
