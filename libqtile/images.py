75dfc940e7aa044a2b758847757887e42940e7fa
Idaho Frost <frostidaho@gmail.com>
2017-02-20 12:32:05 -0500

    Add and update docstrings in libqtile.images
    Closes issue https://github.com/frostidaho/qtile/issues/3

--- libqtile/images.py
+++ libqtile/images.py
@@ -1,3 +1,12 @@
+"""libqtile.images - create cairo surfaces from images
+
+libqtile.images contains code to create and manipulate cairo surfaces and
+objects from image files.
+
+libqtile.images.Loader is intended as the primary tool in this module.
+With Loader(...) you can load individual images (png or svg) by their path,
+or load multiple icons with the Loader.icons method.
+"""
 import io
 import cairocffi
 import os
@@ -7,14 +16,28 @@ from collections import namedtuple, defaultdict
 from .log_utils import logger
 
 class CairoImageSurface(object):
+    """CairoImageSurface is a namespace for functions which convert images to cairo surfs
+
+    Each image type has a classmethod or staticmethod named from_XYZ
+    with a signature compatible with from_XYZ(bytes_string, **kwargs)
+    e.g., from_png(...) and from_svg(...)
+
+    Get a dictionary of available backends from:
+    >>> d_backends = CairoImageSurface.get_backends()
+    """
     @staticmethod
     def from_png(bytes_png, **kwargs):
+        "from_png converts the data from a png file to a cairo ImageSurface."
         return cairocffi.ImageSurface.create_from_png(io.BytesIO(bytes_png))
 
     try:
         cairosvg = __import__('cairosvg')
         @classmethod
         def from_svg(cls, bytes_svg, width=None, height=None, dpi=200, **kwargs):
+            """from_svg converts the data from a svg file to a cairo ImageSurface.
+
+            from_svg(...) depends on the cairosvg library.
+            """
             svg = cls.cairosvg
             tree = svg.parser.Tree(bytestring=bytes_svg)
             png_surf_file = io.BytesIO()
@@ -30,7 +53,8 @@ class CairoImageSurface(object):
             return cls.from_png(png_surf_file.read())
 
     except ImportError:
-        logger.warning("Can't import cairosvg!")
+        logger.warning("Can't import cairosvg! "
+                       "CairoImageSurface.from_svg will not be available.")
 
     @classmethod
     def get_backends(cls):
@@ -45,7 +69,17 @@ class BackendError(Exception):
 def path_to_image_surface(path, **kwargs):
     """Return a cairocffi.ImageSurface corresponding to the image at path
 
-    The filetype suffix needs to be present in image_types or a
+    Args:
+       path (str): the path of the image file
+
+    Kwargs:
+       All keyword arguments are passed directly to the appropriate backend
+       in CairoImageSurface.
+
+    Returns:
+       cairocffi.ImageSurface
+
+    The filetype suffix needs to be present in CairoImageSurface.get_backends() or a
     BackendError will be raised.
     """
     path_lower = path.lower()
@@ -86,7 +120,27 @@ def get_matching_files(dirpath, *names):
 
 LoadedImg = namedtuple('LoadedImg', 'success name path surface')
 class Loader(object):
+    """Loader - create cairo surfaces from image files & icons
+
+    load an image directly with Loader.__call__(). e.g.,
+    >>> ldr = Loader()
+    >>> loaded_image = ldr('/tmp/someimage.png')
+
+    load icons using Loader.icons(). e.g.,
+    >>> ldr = Loader(['/usr/share/icons/Adwaita/24x24', '/usr/share/icons/Adwaita'])
+    >>> loaded_images = ldr.icons('audio-volume-muted', 'audio-volume-low')
+    """
     def __init__(self, icon_dirs=(), width=None, height=None):
+        """Create an instance of Loader
+
+        Args:
+           icon_dirs (:obj:`list` of :obj:`str`, optional):  Search for icons
+               in these directories.
+           width (int, optional): The width of the loaded image surface in pixels.
+           height (int, optional): The height of the loaded image surface in pixels.
+
+        icon_dirs is only used with Loader.icons
+        """
         if not icon_dirs:
             self.icon_dirs = []
         else:
