078695c5a1075e55db82dc1081fbb849f1130351
Idaho Frost <frostidaho@gmail.com>
2017-04-17 19:05:16 -0400

    Refactor test_xcbq

--- test/test_xcbq.py
+++ test/test_xcbq.py
@@ -1,7 +1,7 @@
 import pytest
 from xvfbwrapper import Xvfb
+import xcffib
 from libqtile import xcbq
-
 
 @pytest.fixture(scope='function', autouse=True)
 def xdisplay(request):
@@ -21,16 +21,22 @@ def test_new_window(xdisplay):
     assert geom.width == 640
     assert geom.height == 480
     win.kill_client()
+    with pytest.raises(xcffib.ConnectionException):
+        win.get_geometry()
 
 def test_net_wm_states(xdisplay):
     conn = xcbq.Connection(xdisplay)
     win = conn.create_window(1, 1, 640, 480)
     assert isinstance(win, xcbq.Window)
-    for name in xcbq.net_wm_states:
-        lower_name = name.lstrip('_').lower()
-        val = getattr(win, lower_name)
+    attr_name = lambda x: x.lstrip('_').lower()
+    names = [attr_name(x) for x in xcbq.net_wm_states]
+
+    for name in names:
+        val = getattr(win, name)
         assert val is False
-        setattr(win, lower_name, True)
-        val = getattr(win, lower_name)
+        setattr(win, name, True)
+        val = getattr(win, name)
         assert val is True
     
+    for name in names:
+        assert getattr(win, name) is True
