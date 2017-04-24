01dd8070b0a39b5394e2d1a9ead446bdf36f4362
Idaho Frost <frostidaho@gmail.com>
2017-04-24 15:25:16 -0400

    Start refactoring keymap.py::_QKey

--- libqtile/keymap.py
+++ libqtile/keymap.py
@@ -4,9 +4,24 @@ from six.moves import reduce
 import operator
 
 _X11Key = _namedtuple('_X11Key', ('code', 'mask', 'cfg_key'))
+# _X11Button = _namedtuple('_X11Button', ('code', 'mask', 'cfg_button'))
+
+_X11Input = _namedtuple('_X11Input', ('code', 'mask', 'cfg_obj'))
 
 class Press(object):
     _mask_cache = {}
+    def __init__(self, conn, cfg_obj, **kwargs):
+        self.conn = conn
+        self.cfg_obj = cfg_obj
+        self.modmask = cfg_obj.modmask
+        sa = setattr
+        for k,v in kwargs.items():
+            sa(self, k, v)
+
+    @property
+    def code(self):
+        raise NotImplementedError('need to implement code() property')
+
     def _strs_to_masks(self, modifiers):
         d_keysyms = xcbq.keysyms
         d_modmasks = xcbq.ModMasks
@@ -44,33 +59,14 @@ class Press(object):
         to_masks = self._strs_to_masks
         return reduce(or_, to_masks(modifiers), 0)
 
-
-class _QKey(Press):
-    '''A class used by manager.py
-
-    Not intended to be used in your config.py
-    '''
-
-    def __init__(self, conn, cfg_key):
-        self.conn = conn
-        self.cfg_key = cfg_key
-
-        # self.keysym = xcbq.keysyms[cfg_key.key]
-        self.keysym = cfg_key.keysym
-        self.keycode = conn.keysym_to_keycode(self.keysym)
-        self.modmask = cfg_key.modmask
-        # self.modmask = self.mods_to_mask(cfg_key.modifiers)
-
-    def get_x11_keys(self, *ignore_modifiers):
-        keycode = self.keycode
+    def get_x11(self, *ignore_modifiers):
         modmask = self.modmask
-        X11Key = _X11Key
-        cfg_key = self.cfg_key
+        X11Input = _X11Input
+        cfg_obj = self.cfg_obj
+        code = self.code
 
-        yield X11Key(keycode, modmask, cfg_key)
+        yield X11Input(code, modmask, cfg_obj)
         seen_masks = {modmask}
-        # TODO ignore masks should be some sort of permutation
-        # of all of the ignore modifiers
         ignore_masks = []
         for mods in ignore_modifiers:
             if isinstance(mods, str):
@@ -81,8 +77,31 @@ class _QKey(Press):
             if new_mask in seen_masks:
                 continue
             seen_masks.add(new_mask)
-            yield X11Key(keycode, new_mask, cfg_key)
+            yield X11Input(code, new_mask, cfg_obj)
 
+
+class _QKey(Press):
+    '''A class used by manager.py
+
+    Not intended to be used in your config.py
+    '''
+
+    def __init__(self, conn, cfg_key):
+        super(_QKey, self).__init__(conn, cfg_key)
+
+        # self.keysym = xcbq.keysyms[cfg_key.key]
+        self.keysym = cfg_key.keysym
+        self.keycode = conn.keysym_to_keycode(self.keysym)
+        # self.modmask = cfg_key.modmask
+        # self.modmask = self.mods_to_mask(cfg_key.modifiers)
+
+    @property
+    def code(self):
+        return self.keycode
+
+    def get_x11_keys(self, *ignore_modifiers):
+        for val in self.get_x11(*ignore_modifiers):
+            yield val
 
 def x11_keys(xcbq_conn, *cfg_keys):
     QKey = _QKey
@@ -92,5 +111,12 @@ def x11_keys(xcbq_conn, *cfg_keys):
     )
     for cfg_key in cfg_keys:
         qkey = QKey(xcbq_conn, cfg_key)
-        for xkey in qkey.get_x11_keys(*ignore_modifiers):
+        for xkey in qkey.get_x11(*ignore_modifiers):
             yield xkey
+
+
+class _QClick(Press):
+    def __init__(self, conn, cfg_click):
+        super(_QClick, self).__init__(conn, cfg_click)
+
+        
