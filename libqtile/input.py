9834714a339bb9a0b1a475dfde3fbf790097f855
Idaho Frost <frostidaho@gmail.com>
2017-04-24 21:28:05 -0400

    Add _Grabber class to input.py

--- libqtile/input.py
+++ libqtile/input.py
@@ -36,3 +36,51 @@ def _make_modifiers_to_masks(xcbq_conn):
     return modifiers_to_masks
 
 # modifiers_to_masks = _make_modifiers_to_masks()
+class _Grabber(object):
+    def __init__(self, conn, cfg_input, **kwargs):
+        self.conn = conn
+        self.cfg_input = cfg_input
+        sa = setattr
+        for k, v in kwargs.items():
+            sa(self, k, v)
+
+    def modifiers_to_masks(self, modifiers):
+        try:
+            fn = self.__modifiers_to_masks
+        except AttributeError:
+            fn = _make_modifiers_to_masks(self.conn)
+            _Grabber.__modifiers_to_masks = staticmethod(fn)
+        return list(fn(modifiers))
+
+    @property
+    def modmask(self):
+        return self.cfg_input.modmask
+
+    @property
+    def ignore_modifiers(self):
+        try:
+            return self._ignore_modifiers
+        except AttributeError:
+            ignored = (
+                ('lock', 'Num_Lock'),
+                ('Num_Lock',),
+            )
+            return ignored
+
+    @ignore_modifiers.setter
+    def ignore_modifiers(self, value):
+        self._ignore_modifiers = value
+
+    def grab(self, *args, **kwargs):
+        raise NotImplementedError('must implement grab()')
+
+    def ungrab(self):
+        raise NotImplementedError('must implement ungrab()')
+
+
+# def grab_key(self, keycode, modifiers, owner_events,
+#              pointer_mode, keyboard_mode):
+# def grab_button(self, button, modifiers, owner_events,
+#                 event_mask, pointer_mode, keyboard_mode):
+# def grab_pointer(self, owner_events, event_mask, pointer_mode,
+#                  keyboard_mode, cursor=None):
